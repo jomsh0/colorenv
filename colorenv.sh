1d1e6a76608267bb1f7b207177e1f56a72669cbf
Jonathan Sherrell <dev@jomsh.cc>
2022-04-18 18:17:43 -0500

    Refactor with dynamic scoping, a nifty alternative to returning values from functions by capturing stdout.

--- colorenv.sh
+++ colorenv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-set -e -o pipefail
+set -o pipefail
 
 Blurb="\
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -12,14 +12,12 @@ ANSI_ORDER='black red green yellow blue magenta cyan white'
 pfx_=Ce_
 
 envKeys() {
-    local count clr
-    set -- $ANSI_ORDER
-    for seq in $(seq 0 7); do
-        eval clr=\$$((1+$seq))
-        echo ${pfx_}r${seq}_$clr
-        echo ${pfx_}B${seq}_$clr
+    local clr
+    for clr in $ANSI_ORDER; do
+        echo ${pfx_}$clr;
+        echo ${pfx_}B_$clr;
     done
-    echo ${pfx_}tty_fg ${pfx_}tty_bg
+    echo ${pfx_}foreground ${pfx_}background
 }
 
 regex() {
@@ -30,110 +28,93 @@ regex() {
 
 ENV_KEYS=$(envKeys)
 
-save_map() {
-    local seq key clr st
-    set -- $ANSI_ORDER
-
-    for seq in $(seq 0 7); do
-        eval clr=\$$((1+$seq))
-        for st in r B; do
-            key=__${pfx_}$st$seq
-            eval "$key=$clr"
-        done
-    done
-}
-save_map
-
-_map() { eval echo \$__${pfx_}$1; }
-
 unsetEnv() { echo unset $ENV_KEYS; }
 
 error() {
-    echo '[colorenv]' ERROR: "$@" >&2
+    local fatal
+    if [ "$1" = '-f' ]; then fatal=1; shift; fi
+
+    echo '[colorenv error]:' "$@" >&2
+
+    [ "$fatal" ] && exit 1
     return 1
 }
 
-_validGrep() { grep -oisE '^[0-9a-f]{6}$'; }
-_validVal() { [ "$1" ] && echo "$1" | _validGrep >/dev/null 2>&1; }
-validVal() {
-    local val
-    echo "${1###}" | _validGrep \
-        && return
+validate_cVal() {
+    local v_lo v_hi2 x
+    # : ${cVal?}
+    # tring 'dynamic scoping' out.
+    # caller has to provide a local cVal binding
+    cVal=${1##[!0-9a-f]}
+    # cVal=${cVal%%[!0-9a-f]}
+    cVal=$(printf %x "0x$cVal" 2>/dev/null) || return 1
+    [ ${#cVal} -eq 6 ] && return 0
+    [ ${#cVal} -ne 3 ] && return 1
 
-    echo "${1###}" | sed -e '1{ s|/||g; y/A-F/a-f/ }' | _validGrep \
-        && return
-
-    val=$(pastel format hex "$1") && echo "${val###}"
+    v_lo=${cVal#??} v_hi2=${cVal%?}
+    cVal=
+    for x in ${v_hi2%?} ${v_hi2#?} ${v_lo}; do
+        cVal=$cVal$x$x
+    done
 }
+try_pastel() { cVal=$(pastel format hex "$1") && validate_cVal "$cVal"; }
 
 _setEnv() {
-    local Name Val evnName
+    local cPair cName cVal cVcopy envKey
 
-    for Name; do
-        case "$Name" in
-         *=*)  Val=${Name#*=}; Name=${Name%%=*};;
-           *)  error 'colors need to be in name=value format'; return 1;;
-        esac
+    for cPair; do
+        cVal=${cPair#*=} cName=${cPair%%=*}
+        [ ${#cPair} -gt ${#cVal} ] ||
+           { error 'colors need to be in name=value format'; return 1; }
 
-        Val=$(validVal "$Val") || error "invaid color value: $Val"
-        envName=$(matchName "$Name") || error "invalid color name: $Name"
+        cVcopy=$cVal
+        validate_cVal "$cVal"    ||
+            try_pastel "$cVcopy" ||
+                { error "invaid color value: $cVcopy"; return 1; }
 
-        echo $envName=$Val
+        cName_to_envKey "$cName" ||
+                { error "invalid color name: $cName"; return 1; }
+
+        echo $envKey=$cVal
     done
 }
 
 setEnv() {
-    envStr=$(_setEnv $COLOR_ARGS) && echo "$envStr" && return;
-    error "problem setting environment. no changes made."
+    local envStr
+    envStr=$(_setEnv $COLOR_ARGS) && echo "$envStr" && return
+    error -f 'problem setting environment. no changes made.'
 }
 
 fmt_sh() { echo "$1=$2"; }
 fmt_pretty() { echo "${1##*_} = #$2"; }
-fmt_parse() {
-    local base name st num
-    name=${1##*_} base=${1#$pfx_}
-    base=${base%%_*}
-    st=${base%?} num=${base#?}
-
-    case "$st:$num" in [rB]:[0-7]) ;;
-      *) return 1 ;;
-    esac
-    echo $st $num $name $2
-}
 
 listEnv() {
-    local key val fmtcmd opt all
+    local cVal key fmtcmd all opt OPTIND OPTARG
 
-    unset OPTIND OPTARG
-    while getopts 'apm' opt; do
+    while getopts 'pa' opt; do
     case "$opt" in
        p) fmtcmd=fmt_pretty ;;
-       m) fmtcmd=fmt_parse  ;;
        a) all=all           ;;
     esac
     done
     for key in $ENV_KEYS; do
-        eval val=\$$key
-        _validVal "$val" || [ "$all" ] || continue
-        ${fmtcmd:-fmt_sh} "$key" "$val"
+        eval cVal=\$$key
+        validate_cVal "$cVal" || [ "$all" ] || continue
+        ${fmtcmd:-fmt_sh} "$key" "$cVal"
     done
 }
 
-matchName() {
-    local br clr key
-    echo "$1" | grep -qi bright && br=1
-    clr=$(echo "$1" | grep -sioE "$(regex $ANSI_ORDER purple)") || return 1
-    clr=$(echo "$clr" | tr '[:upper:]' '[:lower:]')
-    [ "$clr" = "purple" ] && clr=magenta
-
-    if [ "$br" ]; then pattern="*_B[0-9]_$clr"
-    else pattern="*_r[0-9]_$clr"
-    fi
-
-    for key in $ENV_KEYS; do
-        case $key in $pattern) echo $key; return 0;; esac
-    done
-    return 1
+cName_to_envKey() {
+    local B_ clr
+    # : ${envKey?}
+    echo "$1" | grep -qiE 'bright|bold' && B_=B_
+    clr=$(echo "$1" |
+        grep -sioE "$(regex $ANSI_ORDER purple foreground background)" |
+        tr '[:upper:]' '[:lower:]'
+    ) || return 1
+    [ "$clr" = purple ] && clr=magenta
+    [ "$B_" ] && [ "$clr" = foreground -o "$clr" = background ] && B_=
+    envKey=${pfx_}${B_}$clr
 }
 
 _help() { cat >&2 <<'###'
@@ -152,35 +133,46 @@ COMMANDS:
 ###
 }
 
-# caller's job to make sure stdout is something reasonable.
-emitEsc() {
+rgb_str() {
+    local cVal v_lo
+    # : ${rgb?}
+    validate_cVal "$1" || return 1
+    v_lo=${cVal#??}
+    rgb=${cVal%????}/${v_lo%??}/${v_lo#??}
+}
+
+_initc() { printf '\e]%s;rgb:%s\e\' "$@"; }
+
+initc() {
     local rgb
-    rgb=$(echo "$2" | grep -osE '[[:xdigit:]]{2}')
-    set -- "$1" $rgb && [ 4 -eq $# ] &&
-        printf '\e]%s;rgb:%s/%s/%s\e\' "$@"
+    [ "$1" -lt 256 ] || return 1
+    rgb_str "$2"     || return 1
+    _initc "4;$1" "$rgb"
+}
+
+initc_fb() {
+    local code rgb
+    rgb_str "$1" && _initc 10 "$rgb"
+    rgb_str "$2" && _initc 11 "$rgb"
 }
 
 escEnv() {
-    local _fg _bg fg_val bg_val
-    listEnv -m | while read st num name val; do
-        case "$st" in  r) base=0;;  B) base=8;;  *) continue;; esac
-        idx=$(($num + $base))
-        emitEsc "4;$idx" "$val"
+    local seq clr fg bg
+    set -- $ANSI_ORDER
+    
+    for seq in $(seq 0 7); do
+        eval clr=\$$((1 + $seq))
+        eval initc $seq \$$pfx_$clr
+        eval initc $((8 + $seq)) \$${pfx_}B_$clr
     done
-
-    eval _fg="\${${pfx_}tty_fg:-r7}"             &&
-        eval fg_val=\$${pfx_}${_fg}_$(_map $_fg) &&
-        _validVal "$fg_val"                      &&
-        emitEsc 10 "$fg_val"
-
-    eval _bg="\${${pfx_}tty_bg:-r0}"             &&
-        eval bg_val=\$${pfx_}${_bg}_$(_map $_bg) &&
-        _validVal "$bg_val"                      &&
-        emitEsc 11 "$bg_val"
+    
+    eval fg=\${${pfx_}foreground:-\$${pfx_}white}
+    eval bg=\${${pfx_}background:-\$${pfx_}black}
+    eval initc_fb "$fg" "$bg"
 }
 
 colorENV() {
-    [ -t 2 ] || error 'stderr must be connected to a terminal'
+    [ -t 2 ] || error -f 'stderr must be connected to a terminal'
     escEnv >&2
 }
 
@@ -198,7 +190,7 @@ stdinColors() {
     set -- $(cat) && echo "$@"
 }
 
-COLOR_ARGS=$(cmdlineColors; echo; stdinColors)
+COLOR_ARGS=$(cmdlineColors "$@"; echo; stdinColors)
 
 case "$1" in
    list*)  Cmd=listEnv   ;;
