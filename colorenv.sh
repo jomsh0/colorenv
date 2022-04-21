546f4b8ee67c4726152dd173fecd8882a1ad6ff0
Jonathan Sherrell <dev@jomsh.cc>
2022-04-21 15:56:16 -0500

    More indentation.

--- colorenv.sh
+++ colorenv.sh
@@ -13,194 +13,194 @@ ce_fatal() { ce_error "$@"; exit 1; }
 ce_error() { echo '[colorenv error]:' "$@" >&2; return 1; }
 
 ce_loadEnv() {
-  local ent key val
-  eval set -- \$$Pfx
-  for ent; do
-    key=${ent%%=*} val=${ent#*=}
-    [ ${#key} -ne ${#ent} ] || { ce_error "bad entry $ent"; continue; }
-    eval ${Pfx}_$key=$val
-  done
+    local ent key val
+    eval set -- \$$Pfx
+    for ent; do
+        key=${ent%%=*} val=${ent#*=}
+        [ ${#key} -ne ${#ent} ] || { ce_error "bad entry $ent"; continue; }
+        eval ${Pfx}_$key=$val
+    done
 }
 
 ce_listEnv() {
-  set | grep -E "^$Pfx\_([0-9]+|$(ce_alternates $ce_KEYS))=" \
-      | sed "s/^$Pfx\_//;s/'//g" \
-      | sort -g
+    set | grep -E "^$Pfx\_([0-9]+|$(ce_alternates $ce_KEYS))=" \
+        | sed "s/^$Pfx\_//;s/'//g" \
+        | sort -g
 }
 
 ce_saveEnv() { echo $Pfx=\'$(ce_listEnv)\'; }
 
 ce_validate_cVal() {
-  local work v_lo v_hi2 x
-  # caller has to provide a local cVal binding
+    local work v_lo v_hi2 x
+    # caller has to provide a local cVal binding
 
-  cVal=${1###}
-  [ ${#cVal} -eq 8 ] && cVal=$(echo "$cVal" | sed 's^/^^g')
+    cVal=${1###}
+    [ ${#cVal} -eq 8 ] && cVal=$(echo "$cVal" | sed 's^/^^g')
 
-  work=$(printf %0${#cVal}x "0x$cVal" 2>/dev/null) || return 1
+    work=$(printf %0${#cVal}x "0x$cVal" 2>/dev/null) || return 1
 
-  cVal=$work
-  [ ${#cVal} -eq 6 ] && return 0
-  [ ${#cVal} -ne 3 ] && return 1
+    cVal=$work
+    [ ${#cVal} -eq 6 ] && return 0
+    [ ${#cVal} -ne 3 ] && return 1
 
-  v_lo=${cVal#??} v_hi2=${cVal%?}
-  cVal=
-  for x in ${v_hi2%?} ${v_hi2#?} ${v_lo}; do
-    cVal=$cVal$x$x
-  done
+    v_lo=${cVal#??} v_hi2=${cVal%?}
+    cVal=
+    for x in ${v_hi2%?} ${v_hi2#?} ${v_lo}; do
+        cVal=$cVal$x$x
+    done
 }
 
 ce_try_pastel() {
-  local work
-  work=$(pastel format hex "$1") && ce_validate_cVal "$work"
+    local work
+    work=$(pastel format hex "$1") && ce_validate_cVal "$work"
 }
 
 _ce_setEnv() {
-  local cPair cName cVal envKey idx
+    local cPair cName cVal envKey idx
 
-  idx=0
-  for cPair; do
-    case "$cPair" in
-    *=*) cVal=${cPair#*=} cName=${cPair%%=*}
-         ce_matchKey "$cName" || ce_error "unable to match $cName" ;;
-      *) cVal=$cPair; envKey=$idx; idx=$((1 + idx))
-         [ $idx -lt 256 ] || ce_error 'only indices 0-255 allowed' ;;
-    esac || continue
+    idx=0
+    for cPair; do
+      case "$cPair" in
+        *=*) cVal=${cPair#*=} cName=${cPair%%=*}
+             ce_matchKey "$cName" || ce_error "unable to match $cName" ;;
+          *) cVal=$cPair; envKey=$idx; idx=$((1 + idx))
+             [ $idx -lt 256 ] || ce_error 'only indices 0-255 allowed' ;;
+      esac || continue
 
-    # These don't need to be validated as colors
-    if ! case " $ce_KEYS_etc " in (*\ $envKey\ *) true;; *) false;; esac &&
-            ! ce_validate_cVal "$cVal" &&
-            ! ce_try_pastel    "$cVal"  ;
-    then { ce_error "invalid color value: $cVal"; continue; } fi
+      # These don't need to be validated as colors
+      if ! case " $ce_KEYS_etc " in (*\ $envKey\ *) true;; *) false;; esac &&
+              ! ce_validate_cVal "$cVal" &&
+              ! ce_try_pastel    "$cVal"  ;
+      then { ce_error "invalid color value: $cVal"; continue; } fi
 
-    echo ${Pfx}_$envKey=$cVal
-  done
+      echo ${Pfx}_$envKey=$cVal
+    done
 }
 
 ce_mapFB() {
-  local cVal match st
-  if ce_validate_cVal "$fg"; then
-    match=$(ce_listEnv | grep -s -m1 =$cVal\$) && fg=${match%%=*}
-  fi
-  st=$?
-  if ce_validate_cVal "$bg"; then
-    match=$(ce_listEnv | grep -s -m1 =$cVal\$) && bg=${match%%=*}
-  fi || return 1
-  return $st
+    local cVal match st
+    if ce_validate_cVal "$fg"; then
+        match=$(ce_listEnv | grep -s -m1 =$cVal\$) && fg=${match%%=*}
+    fi
+    st=$?
+    if ce_validate_cVal "$bg"; then
+        match=$(ce_listEnv | grep -s -m1 =$cVal\$) && bg=${match%%=*}
+    fi || return 1
+    return $st
 }
 
 ce_unmapFB() {
-  local cVal st
+    local cVal st
 
-  eval cVal=\$$Pfx\_$fg && ce_validate_cVal "$cVal" && fg=$cVal
-  st=$?
-  eval cVal=\$$Pfx\_$bg && ce_validate_cVal "$cVal" && bg=$cVal
-  return $((st + $?))
+    eval cVal=\$$Pfx\_$fg && ce_validate_cVal "$cVal" && fg=$cVal
+    st=$?
+    eval cVal=\$$Pfx\_$bg && ce_validate_cVal "$cVal" && bg=$cVal
+    return $((st + $?))
 }
 
 ce_setEnv() {
-  local envStr fg bg cVal
+    local envStr fg bg cVal
 
-  envStr=$(_ce_setEnv $COLOR_ARGS) || ce_fatal 'problem setting environment. no changes made.'
+    envStr=$(_ce_setEnv $COLOR_ARGS) || ce_fatal 'problem setting environment. no changes made.'
 
-  case "$ce_autofb" in
-    K|M)  eval  fg=\$$Pfx\_foreground  bg=\$$Pfx\_background  ;;
-  esac
+    case "$ce_autofb" in
+      K|M)  eval  fg=\$$Pfx\_foreground  bg=\$$Pfx\_background  ;;
+    esac
 
-  [ "$ce_autofb" = M ] && ce_mapFB
+    [ "$ce_autofb" = M ] && ce_mapFB
 
-  eval "$envStr"
+    eval "$envStr"
 
-  [ "$ce_autofb" ] || return
+    [ "$ce_autofb" ] || return
 
-  case "$ce_autofb" in
-    M)  ce_unmapFB ;;
-    L)  eval fg=\$$Pfx\_0 bg=\$$Pfx\_7 ;;
-    D)  eval fg=\$$Pfx\_7 bg=\$$Pfx\_0 ;;
-  esac
+    case "$ce_autofb" in
+      M)  ce_unmapFB ;;
+      L)  eval fg=\$$Pfx\_0 bg=\$$Pfx\_7 ;;
+      D)  eval fg=\$$Pfx\_7 bg=\$$Pfx\_0 ;;
+    esac
 
-  if ce_validate_cVal "$fg"; then eval $Pfx\_foreground=$cVal
-  else ce_error "[auto fg/bg = $ce_autofb] couldn't validate fg=$fg"; fi
+    if ce_validate_cVal "$fg"; then eval $Pfx\_foreground=$cVal
+    else ce_error "[auto fg/bg = $ce_autofb] couldn't validate fg=$fg"; fi
 
-  if ce_validate_cVal "$bg"; then eval $Pfx\_background=$cVal
-  else ce_error "[auto fg/bg = $ce_autofb] couldn't validate bg=$bg"; fi
+    if ce_validate_cVal "$bg"; then eval $Pfx\_background=$cVal
+    else ce_error "[auto fg/bg = $ce_autofb] couldn't validate bg=$bg"; fi
 }
 
 ce_matchKey() {
-  local off clr e
+    local off clr e
 
-  off=0
-  echo "$1" | grep -qiE 'bright|bold' && off=8
+    off=0
+    echo "$1" | grep -qiE 'bright|bold' && off=8
 
-  clr=$(echo "$1" | grep -sioE "$(ce_alternates $ANSI_ORDER purple)") \
-      || clr=$1
-  clr=$(echo "$clr" | tr '[:upper:]' '[:lower:]')
+    clr=$(echo "$1" | grep -sioE "$(ce_alternates $ANSI_ORDER purple)") \
+        || clr=$1
+    clr=$(echo "$clr" | tr '[:upper:]' '[:lower:]')
 
-  [ "$clr" = purple ] && clr=magenta
+    [ "$clr" = purple ] && clr=magenta
 
-  case "$clr" in
-    black)  envKey=$off ;;
-      red)  envKey=$((1 + off)) ;;
-    green)  envKey=$((2 + off)) ;;
-   yellow)  envKey=$((3 + off)) ;;
-     blue)  envKey=$((4 + off)) ;;
-  magenta)  envKey=$((5 + off)) ;;
-     cyan)  envKey=$((6 + off)) ;;
-    white)  envKey=$((7 + off)) ;;
-[0-9] | [0-9][0-9]) envKey=$clr   ;;
-        *)  false ;;
-  esac && return
+    case "$clr" in
+      black)  envKey=$off ;;
+        red)  envKey=$((1 + off)) ;;
+      green)  envKey=$((2 + off)) ;;
+     yellow)  envKey=$((3 + off)) ;;
+       blue)  envKey=$((4 + off)) ;;
+    magenta)  envKey=$((5 + off)) ;;
+       cyan)  envKey=$((6 + off)) ;;
+      white)  envKey=$((7 + off)) ;;
+  [0-9] | [0-9][0-9]) envKey=$clr   ;;
+          *)  false ;;
+    esac && return
 
-  # misc. whitelist
-  eval "case $clr in
-    $(ce_alternates $ce_KEYS)) envKey=$clr ;;
-    *) return 1 ;;
-    esac"
+    # misc. whitelist
+    eval "case $clr in
+        $(ce_alternates $ce_KEYS)) envKey=$clr ;;
+        *) return 1 ;;
+        esac"
 }
 
 ce_rgb_str() {
-  local cVal v_lo
-  ce_validate_cVal "$1" || return 1
-  v_lo=${cVal#??}
-  rgb=${cVal%????}/${v_lo%??}/${v_lo#??}
+    local cVal v_lo
+    ce_validate_cVal "$1" || return 1
+    v_lo=${cVal#??}
+    rgb=${cVal%????}/${v_lo%??}/${v_lo#??}
 }
 
 _ce_initc() { printf '\e]%s;rgb:%s\e\' "$@"; }
 
 ce_initc() {
-  local rgb
-  [ "$1" -lt 256 ] || return 1
-  ce_rgb_str "$2"     || return 1
-  _ce_initc "4;$1" "$rgb"
+    local rgb
+    [ "$1" -lt 256 ] || return 1
+    ce_rgb_str "$2"     || return 1
+    _ce_initc "4;$1" "$rgb"
 }
 
 ce_initc_fb() {
-  local code rgb
-  ce_rgb_str "$1" && _ce_initc 10 "$rgb"
-  ce_rgb_str "$2" && _ce_initc 11 "$rgb"
+    local code rgb
+    ce_rgb_str "$1" && _ce_initc 10 "$rgb"
+    ce_rgb_str "$2" && _ce_initc 11 "$rgb"
 }
 
 ce_escEnv() {
-  local seq clr fg bg
+    local seq clr fg bg
   
-  for seq in $(seq 0 15); do
-    eval ce_initc $seq \$${Pfx}_$seq
-    eval ce_initc $((8 + seq)) \$${Pfx}_$((8 + seq))
-  done
+    for seq in $(seq 0 15); do
+        eval ce_initc $seq \$${Pfx}_$seq
+        eval ce_initc $((8 + seq)) \$${Pfx}_$((8 + seq))
+    done
   
-  eval fg=\${${Pfx}_foreground:-\$${Pfx}_7}
-  eval bg=\${${Pfx}_background:-\$${Pfx}_0}
-  eval ce_initc_fb "$fg" "$bg"
+    eval fg=\${${Pfx}_foreground:-\$${Pfx}_7}
+    eval bg=\${${Pfx}_background:-\$${Pfx}_0}
+    eval ce_initc_fb "$fg" "$bg"
 }
 
 ce_colorENV() {
-  [ -t 2 ] || ce_fatal 'stderr must be connected to a terminal'
-  ce_escEnv >&2
+    [ -t 2 ] || ce_fatal 'stderr must be connected to a terminal'
+    ce_escEnv >&2
 }
 
 ce_stdinColors() {
-  [ -t 0 ] && return 0
-  set -- $(cat) && echo "$@"
+    [ -t 0 ] && return 0
+    set -- $(cat) && echo "$@"
 }
 
 t_a_b=$(printf '\e[1m') t_a_u=$(printf '\e[4m') t_a_0=$(printf '\e[0m')
@@ -222,31 +222,31 @@ EOF
 ce_explodeOpt() { echo -n "$1" | (while read -r -n1 char; do echo -$char; done); }
 
 ce_parseOpts() {
-  local orig opt;  orig=$#
+    local orig opt;  orig=$#
 
-  while [ $# -gt 0 ] && [ -z "${1%%-*}" ]; do
-    opt=${1#-}; shift
-    case "$opt" in
-?[a-zA-Z]*) set -- $(ce_explodeOpt "$opt") "$@" ;;
-         l) ce_list=1  ;;
-         a) ce_apply=1 ;;
-         r) ce_reset=1 ;;
-   L|D|K|M) ce_autofb=$opt ;;
-      h|\?) ce_help    ;;
-         *) return 1 ;;
-    esac
-  done
-  ce_SHIFTS=$((orig - $#))
+    while [ $# -gt 0 ] && [ -z "${1%%-*}" ]; do
+      opt=${1#-}; shift
+      case "$opt" in
+  ?[a-zA-Z]*) set -- $(ce_explodeOpt "$opt") "$@" ;;
+           l) ce_list=1  ;;
+           a) ce_apply=1 ;;
+           r) ce_reset=1 ;;
+     L|D|K|M) ce_autofb=$opt ;;
+        h|\?) ce_help    ;;
+           *) return 1 ;;
+      esac
+    done
+    ce_SHIFTS=$((orig - $#))
 }
 if [ $(basename "$0") = colorenv.sh ]; then
-  ce_parseOpts "$@"; shift $ce_SHIFTS
+    ce_parseOpts "$@"; shift $ce_SHIFTS
 
-  [ "$ce_reset" ] || ce_loadEnv
+    [ "$ce_reset" ] || ce_loadEnv
 
-  COLOR_ARGS=$(echo "$@"; ce_stdinColors)
-  ce_setEnv
+    COLOR_ARGS=$(echo "$@"; ce_stdinColors)
+    ce_setEnv
 
-  [ "$ce_apply" ] &&  ce_colorENV
-  [ "$ce_list"  ] &&  ce_listEnv &&  exit # keep stdout coherent
-  ce_saveEnv
+    [ "$ce_apply" ] &&  ce_colorENV
+    [ "$ce_list"  ] &&  ce_listEnv &&  exit # keep stdout coherent
+    ce_saveEnv
 fi
--- tweak.sh
+++ tweak.sh
@@ -12,49 +12,49 @@ tw_andSave() { eval export $(echo $("$@") | $CLenv); }
 tw_capture() { $CLenv -l | tw_array; }
 
 tw_lines() {
-  local seq
-  [ "$1" -gt 0 ] || return 1
-  for seq in $(seq 1 "$1"); do read line; echo $line; done
+    local seq
+    [ "$1" -gt 0 ] || return 1
+    for seq in $(seq 1 "$1"); do read line; echo $line; done
 }
 
 tw_Select() {
-  local hi lo size nxt s
+    local hi lo size nxt s
   
-  hi=-1   # zero index
-  for s in $(tw_seldecode $sel); do
-    nxt=${s%%[!0-9]*}
-    size=$((nxt - hi - 1)) 2>/dev/null ||
-      { tw_error "decoded selection $s invalid"; return 1; }
+    hi=-1   # zero index
+    for s in $(tw_seldecode $sel); do
+        nxt=${s%%[!0-9]*}
+        size=$((nxt - hi - 1)) 2>/dev/null ||
+            { tw_error "decoded selection $s invalid"; return 1; }
 
-    if ! [ "$1" ] && [ $size -gt 0 ]; then
-      tw_lines $size >/dev/null
-    fi
+        if ! [ "$1" ] && [ $size -gt 0 ]; then
+            tw_lines $size >/dev/null
+        fi
 
-    lo=${s%[!0-9]*}  hi=${s#*[!0-9]}
-    size=$((hi - lo + 1)) 2>/dev/null ||
-      { tw_error "decoded selection $s invalid"; return 1; }
+        lo=${s%[!0-9]*}  hi=${s#*[!0-9]}
+        size=$((hi - lo + 1)) 2>/dev/null ||
+            { tw_error "decoded selection $s invalid"; return 1; }
 
-    [ $size -gt 0 ]  ||  continue
+        [ $size -gt 0 ]  ||  continue
 
-    if ! [ "$1" ]; then
-      tw_lines $size
-    else for ln in $(tw_lines $size); do
-      [ $lo -le $hi ] || continue
-      echo $lo=$ln
-      lo=$((lo + 1))
-    done fi
-  done
+        if ! [ "$1" ]; then
+            tw_lines $size
+        else for ln in $(tw_lines $size); do
+            [ $lo -le $hi ] || continue
+            echo $lo=$ln
+            lo=$((lo + 1))
+        done fi
+    done
 }
 
 tw_selsplit() {
-  local IFS s; IFS=$IFS,
-  set -- $@; IFS=${IFS%,}
-  for s; do
-    case "$s" in
-     *[!0-9-]*) echo $s | { while read -n1 c; do echo $c; done; };;
-             *) echo $s ;;
-    esac
-  done
+    local IFS s; IFS=$IFS,
+    set -- $@; IFS=${IFS%,}
+    for s; do
+      case "$s" in
+       *[!0-9-]*) echo $s | { while read -n1 c; do echo $c; done; };;
+               *) echo $s ;;
+      esac
+    done
 }
 
 SEL_C_BRI=9-14
@@ -65,35 +65,35 @@ SEL_BW=$SEL_BW_REG,$SEL_BW_BRI
 SEL_ALL=$SEL_C,$SEL_BW
 
 tw_seldecode() {
-  local ansi sel
-  for sel in $(tw_selsplit $@); do
-    # numeric range case
-    case "$sel" in *[!0-9-]*);; *) echo $sel; continue ;; esac
+    local ansi sel
+    for sel in $(tw_selsplit $@); do
+        # numeric range case
+        case "$sel" in *[!0-9-]*);; *) echo $sel; continue ;; esac
 
-    ansi=  # color initial cases
-    case "$sel" in
-     k|K) ansi=0  ;;  # black
-     w|W) ansi=7  ;;  # white
-     r|R) ansi=1  ;;  # red
-     g|G) ansi=2  ;;  # green
-     y|Y) ansi=3  ;;  # yellow
-     b|B) ansi=4  ;;  # blue
-     m|M) ansi=5  ;;  # magenta
-     c|C) ansi=6  ;;  # cyan
-       *) false   ;;
-    esac  &&
-        { [ "$sel" \> Z ] || ansi=$((ansi + 8))
-          echo $ansi; continue; }
+        ansi=  # color initial cases
+        case "$sel" in
+         k|K) ansi=0  ;;  # black
+         w|W) ansi=7  ;;  # white
+         r|R) ansi=1  ;;  # red
+         g|G) ansi=2  ;;  # green
+         y|Y) ansi=3  ;;  # yellow
+         b|B) ansi=4  ;;  # blue
+         m|M) ansi=5  ;;  # magenta
+         c|C) ansi=6  ;;  # cyan
+           *) false   ;;
+        esac  &&
+            { [ "$sel" \> Z ] || ansi=$((ansi + 8))
+              echo $ansi; continue; }
     
-    # wildcard cases
-    case "$sel" in
-     %) tw_selsplit $SEL_ALL    ;;
-     @) tw_selsplit $SEL_C_REG  ;;
-     ^) tw_selsplit $SEL_C_BRI  ;;
-     =) tw_selsplit $SEL_C      ;;
-     _) tw_selsplit $SEL_BW     ;;
-    esac
-  done | sort -g | uniq
+        # wildcard cases
+        case "$sel" in
+         %) tw_selsplit $SEL_ALL    ;;
+         @) tw_selsplit $SEL_C_REG  ;;
+         ^) tw_selsplit $SEL_C_BRI  ;;
+         =) tw_selsplit $SEL_C      ;;
+         _) tw_selsplit $SEL_BW     ;;
+        esac
+    done | sort -g | uniq
 }
 
 tw_usage() { cat >&2; exit 1; } <<EOF
@@ -133,53 +133,53 @@ EOF
 tw_error() { echo '[error]: ' "$@" >&2; return 1; }
 
 tw_decodeOpt() {
-  local work prop
-  op= sign=  sel=${1##*[:0-9]} work=${1#?}
-  prop=${1%$work} work=${work%$sel}
-  val=${work#?}   sign=${work%%[!=+-]*}
-  val=${val%:}
+    local work prop
+    op= sign=  sel=${1##*[:0-9]} work=${1#?}
+    prop=${1%$work} work=${work%$sel}
+    val=${work#?}   sign=${work%%[!=+-]*}
+    val=${val%:}
 
-  if ! [ ${#prop} -eq 1  -a  ${#sign} -eq 1 ]; then
-    tw_error "couldn't parse $1"; return 1
-  fi
+    if ! [ ${#prop} -eq 1  -a  ${#sign} -eq 1 ]; then
+        tw_error "couldn't parse $1"; return 1
+    fi
 
-  [ "$sign" = '=' ]  &&  { op=set; sign=; }
+    [ "$sign" = '=' ]  &&  { op=set; sign=; }
   
-  case "$prop" in S|L)
-    case "$val" in
-      0.*|.*)       ;;
-         100) val=1 ;;
-        ??|?) val=$(printf .%02d $val) ;;
-    esac ;;
-  esac
+    case "$prop" in S|L)
+       case "$val" in
+       0.*|.*)       ;;
+          100) val=1 ;;
+         ??|?) val=$(printf .%02d $val) ;;
+      esac ;;
+    esac
 
-  case "$prop" in
-    H) prop=hsl-hue        op=${op:-rotate}   sel=${sel:-$SEL_C} ;;
-    S) prop=hsl-saturation op=${op:-saturate} sel=${sel:-$SEL_C} ;;
-    L) prop=hsl-lightness  op=${op:-lighten}   ;;
-    R) prop=red    op=set  sel=${sel:-$SEL_C}  ;;
-    G) prop=green  op=set  sel=${sel:-$SEL_C}  ;;
-    B) prop=blue   op=set  sel=${sel:-$SEL_C}  ;;
-    X) op=mix   ;;
-    *) return 1 ;;
-  esac
+    case "$prop" in
+      H) prop=hsl-hue        op=${op:-rotate}   sel=${sel:-$SEL_C} ;;
+      S) prop=hsl-saturation op=${op:-saturate} sel=${sel:-$SEL_C} ;;
+      L) prop=hsl-lightness  op=${op:-lighten}   ;;
+      R) prop=red    op=set  sel=${sel:-$SEL_C}  ;;
+      G) prop=green  op=set  sel=${sel:-$SEL_C}  ;;
+      B) prop=blue   op=set  sel=${sel:-$SEL_C}  ;;
+      X) op=mix   ;;
+      *) return 1 ;;
+    esac
 
-  if [ "$op" = set ]; then op="set $prop"; fi
+    if [ "$op" = set ]; then op="set $prop"; fi
 }
 
 tw_Cmd() {
-  local sel op val sign cat
-  tw_decodeOpt "$@" || return 1
-  tw_Select | $Past $op $sign$val | $Pfmt | tw_Select inv
+    local sel op val sign cat
+    tw_decodeOpt "$@" || return 1
+    tw_Select | $Past $op $sign$val | $Pfmt | tw_Select inv
 }
 
 tw_main() {
-  tw_init
-  while [ $# -gt 0 ]; do
-    tw_save $(tw_capture | tw_Cmd "$1")
-    shift
-  done
-  tw_save -aD
+    tw_init
+    while [ $# -gt 0 ]; do
+        tw_save $(tw_capture | tw_Cmd "$1")
+        shift
+    done
+    tw_save -aD
 }
 
 while [ $# -gt 0  -a  -z "${1%%-*}" ]; do
@@ -191,5 +191,5 @@ while [ $# -gt 0  -a  -z "${1%%-*}" ]; do
 done
 
 if [ $(basename "$0") = tweak.sh ]; then
-  tw_main "$@"
+    tw_main "$@"
 fi
